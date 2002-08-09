16f1db824f68737283888c9beb7e644597fc07e7
nalin <devnull@localhost>
2002-08-09 09:57:48 -0500

    handle unexpectedly long responses

--- dnsclient.c
+++ dnsclient.c
@@ -23,6 +23,7 @@
 #include <netinet/in.h>
 #include <ctype.h>
 #include <limits.h>
+#include <resolv.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -71,17 +72,17 @@ dns_name_to_label(const char *name, unsigned char *label, size_t size)
 	const char *p, *q;
 	unsigned char length, *out;
 
-	if(strlen(name) + 1 >= size - 1) {
+	if (strlen(name) + 1 >= size - 1) {
 		return 0;
 	}
 
 	p = name;
 	out = label;
-	while(p && (*p != '\0')) {
+	while (p && (*p != '\0')) {
 		int more;
 		q = strchr(p, '.');
 
-		if(q == NULL) {
+		if (q == NULL) {
 			more = 0;
 			length = strlen(p);
 		} else {
@@ -116,11 +117,11 @@ dns_format_query(const char *query, u_int16_t qclass, u_int16_t qtype,
 
 	memset(&qlabel, 0, sizeof(qlabel));
 	length = dns_name_to_label(query, qlabel, sizeof(qlabel));
-	if(length == 0) {
+	if (length == 0) {
 		return 0;
 	}
 
-	if(length + sizeof(header) > qbuf_len) {
+	if (length + 4 + sizeof(header) > qbuf_len) {
 		return 0;
 	}
 
@@ -141,8 +142,8 @@ dns_format_query(const char *query, u_int16_t qclass, u_int16_t qtype,
 }
 
 static size_t
-dns_parse_label(const unsigned char *label, size_t length,
-		const unsigned char *base,
+dns_parse_label(const unsigned char *label,
+		const unsigned char *base, size_t length,
 		unsigned char *output, size_t output_length,
 		const unsigned char **next)
 {
@@ -150,8 +151,8 @@ dns_parse_label(const unsigned char *label, size_t length,
 	int update = 0;
 	const unsigned char *p;
 
-	if(label == NULL) {
-		if(next) {
+	if (label == NULL) {
+		if (next) {
 			*next = NULL;
 		}
 		return 0;
@@ -159,8 +160,8 @@ dns_parse_label(const unsigned char *label, size_t length,
 
 	p = label;
 	update = 1;
-	while(p && *p) {
-		if(*p & 0xc0) {
+	while (p && (p < base + length) && *p) {
+		if (*p & 0xc0) {
 			p = base + ((p[0] & 0x3f) << 8) + p[1];
 			continue;
 		}
@@ -168,8 +169,10 @@ dns_parse_label(const unsigned char *label, size_t length,
 		p += (*p + 1);
 	}
 
-	if((p == NULL) || (ret >= output_length)) {
-		*next = NULL;
+	if ((p == NULL) || (p >= base + length) || (ret >= output_length)) {
+		if (next) {
+			*next = NULL;
+		}
 		return 0;
 	}
 
@@ -177,16 +180,16 @@ dns_parse_label(const unsigned char *label, size_t length,
 	update = 1;
 	p = label;
 	memset(output, '\0', output_length);
-	while(p && *p) {
-		if(*p & 0xc0) {
+	while (p && *p) {
+		if (*p & 0xc0) {
 			p = base + ((p[0] & 0x3f) << 8) + p[1];
 			ret += 2;
-			if(update) {
+			if (update) {
 				update = 0;
 			}
 			continue;
 		}
-		if(update) {
+		if (update) {
 			ret += (*p + 1);
 		}
 		strncat(output, p + 1, *p);
@@ -194,17 +197,21 @@ dns_parse_label(const unsigned char *label, size_t length,
 		p += (*p + 1);
 	}
 
-	if(next) {
+	if (next) {
 		*next = label + (update ? ret + 1 : ret);
 	}
 
 	return update ? ret + 1 : ret;
 }
 
-static void
-dns_parse_a(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_a(const unsigned char *rr, const unsigned char *base, size_t max,
 	    struct dns_rr *res)
 {
+	if (rr + 4 > base + max) {
+		res->dns_rdata.a.address = 0;
+		return 0;
+	}
 	res->dns_rdata.a.address = (rr[0] << 24) |
 				   (rr[1] << 16) |
 				   (rr[2] <<  8) |
@@ -212,26 +219,29 @@ dns_parse_a(const unsigned char *rr, const unsigned char *base,
 #ifdef DEBUG_DNSCLIENT
 	printf("A = %d.%d.%d.%d.\n", rr[0], rr[1], rr[2], rr[3]);
 #endif
+	return 4;
 }
 
-static void
-dns_parse_domain(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_domain(const unsigned char *rr, const unsigned char *base, size_t max,
 		 struct dns_rr *res, const char **ret)
 {
 	unsigned char buf[DOMAIN_MAX];
-	if(dns_parse_label(rr, res->dns_rlength, base,
-			   buf, sizeof(buf), NULL)) {
+	if (dns_parse_label(rr, base, max, buf, sizeof(buf), NULL)) {
 		*ret = strdup(buf);
+		return strlen(*ret);
 	}
+	return 0;
 }
 
 static const unsigned char *
-dns_parse_text(const unsigned char *rr, const unsigned char *base,
+dns_parse_text(const unsigned char *rr, const unsigned char *base, size_t max,
 	       struct dns_rr *res, const char **ret)
 {
 	unsigned char buf[DOMAIN_MAX];
-	if(*rr) {
-		if(*rr < res->dns_rlength) {
+	if (*rr) {
+		if ((*rr < res->dns_rlength) && (*rr < sizeof(buf)) &&
+		    (rr + *rr <  base + max)) {
 			memset(buf, '\0', sizeof(buf));
 			strncpy(buf, rr + 1, *rr);
 			*ret = strdup(buf);
@@ -241,39 +251,51 @@ dns_parse_text(const unsigned char *rr, const unsigned char *base,
 	return NULL;
 }
 
-static void
-dns_parse_ns(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_ns(const unsigned char *rr, const unsigned char *base, size_t max,
 	     struct dns_rr *res)
 {
-	dns_parse_domain(rr, base, res, &res->dns_rdata.ns.nsdname);
+	int ret;
+	ret = dns_parse_domain(rr, base, max, res, &res->dns_rdata.ns.nsdname);
 #ifdef DEBUG_DNSCLIENT
 	printf("NS DNAME = \"%s\".\n", res->dns_rdata.ns.nsdname);
 #endif
+	return ret;
 }
 
-static void
-dns_parse_cname(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_cname(const unsigned char *rr, const unsigned char *base, size_t max,
 		struct dns_rr *res)
 {
-	dns_parse_domain(rr, base, res, &res->dns_rdata.cname.cname);
+	int ret;
+	ret = dns_parse_domain(rr, base, max, res, &res->dns_rdata.cname.cname);
 #ifdef DEBUG_DNSCLIENT
 	printf("CNAME = \"%s\".\n", res->dns_rdata.cname.cname);
 #endif
+	return ret;
 }
 
-static void
-dns_parse_soa(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_soa(const unsigned char *rr, const unsigned char *base, size_t max,
 	      struct dns_rr *res)
 {
 	char buf[2048];
+	int ret;
 
-	if(dns_parse_label(rr, 20, base, buf, sizeof(buf), &rr)) {
+	if (rr + 20 > base + max) {
+		return 0;
+	}
+	if (dns_parse_label(rr, base, max, buf, sizeof(buf), &rr)) {
 		res->dns_rdata.soa.mname = strdup(buf);
+	} else {
+		return 0;
 	}
-	if(dns_parse_label(rr, 20, base, buf, sizeof(buf), &rr)) {
+	if (dns_parse_label(rr, base, max, buf, sizeof(buf), &rr)) {
 		res->dns_rdata.soa.rname = strdup(buf);
+	} else {
+		return 0;
 	}
-	if(rr) {
+	if (rr) {
 		res->dns_rdata.soa.serial = (rr[0] << 24) + (rr[1] << 16) + (rr[2] << 8) + rr[3];
 		res->dns_rdata.soa.refresh = (rr[4] << 24) + (rr[5] << 16) + (rr[6] << 8) + rr[7];
 		res->dns_rdata.soa.retry = (rr[8] << 24) + (rr[9] << 16) + (rr[10] << 8) + rr[11];
@@ -292,25 +314,26 @@ dns_parse_soa(const unsigned char *rr, const unsigned char *base,
 }
 
 static void
-dns_parse_null(const unsigned char *rr, const unsigned char *base,
+dns_parse_null(const unsigned char *rr, const unsigned char *base, size_t max,
 	       struct dns_rr *res)
 {
 	/* um, yeah */
 }
 
-static void
-dns_parse_wks(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_wks(const unsigned char *rr, const unsigned char *base, size_t max,
 	      struct dns_rr *res)
 {
+	return 0;
 }
 
-static void
-dns_parse_hinfo(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_hinfo(const unsigned char *rr, const unsigned char *base, size_t max,
 		struct dns_rr *res)
 {
-	rr = dns_parse_text(rr, base, res, &res->dns_rdata.hinfo.cpu);
-	if(rr) {
-		dns_parse_text(rr, base, res, &res->dns_rdata.hinfo.os);
+	rr = dns_parse_text(rr, base, max, res, &res->dns_rdata.hinfo.cpu);
+	if (rr) {
+		dns_parse_text(rr, base, max, res, &res->dns_rdata.hinfo.os);
 	}
 #ifdef DEBUG_DNSCLIENT
 	printf("HINFO(cpu) = \"%s\".\n", res->dns_rdata.hinfo.cpu);
@@ -318,46 +341,53 @@ dns_parse_hinfo(const unsigned char *rr, const unsigned char *base,
 #endif
 }
 
-static void
-dns_parse_mx(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_mx(const unsigned char *rr, const unsigned char *base, size_t max,
 	     struct dns_rr *res)
 {
+	if (rr + 2 > base + max) {
+		return 0;
+	}
 	res->dns_rdata.mx.preference = (rr[0] << 8) | rr[1];
-	dns_parse_domain(rr + 2, base, res, &res->dns_rdata.mx.exchange);
+	dns_parse_domain(rr + 2, base, max, res, &res->dns_rdata.mx.exchange);
 #ifdef DEBUG_DNSCLIENT
 	printf("MX(exchanger) = \"%s\".\n", res->dns_rdata.mx.exchange);
 	printf("MX(preference) = %d.\n", res->dns_rdata.mx.preference);
 #endif
+	return 2;
 }
 
 static void
-dns_parse_txt(const unsigned char *rr, const unsigned char *base,
+dns_parse_txt(const unsigned char *rr, const unsigned char *base, size_t max,
 	      struct dns_rr *res)
 {
-	dns_parse_text(rr, base, res, &res->dns_rdata.txt.data);
+	dns_parse_text(rr, base, max, res, &res->dns_rdata.txt.data);
 #ifdef DEBUG_DNSCLIENT
 	printf("TXT = \"%s\".\n", res->dns_rdata.txt.data);
 #endif
 }
 
 static void
-dns_parse_ptr(const unsigned char *rr, const unsigned char *base,
+dns_parse_ptr(const unsigned char *rr, const unsigned char *base, size_t max,
 	      struct dns_rr *res)
 {
-	dns_parse_domain(rr, base, res, &res->dns_rdata.ptr.ptrdname);
+	dns_parse_domain(rr, base, max, res, &res->dns_rdata.ptr.ptrdname);
 #ifdef DEBUG_DNSCLIENT
 	printf("PTR = \"%s\".\n", res->dns_rdata.ptr.ptrdname);
 #endif
 }
 
-static void
-dns_parse_srv(const unsigned char *rr, const unsigned char *base,
+static int
+dns_parse_srv(const unsigned char *rr, const unsigned char *base, size_t max,
 	      struct dns_rr *res)
 {
+	if (rr + 6 > base + max) {
+		return 0;
+	}
 	res->dns_rdata.srv.priority = (rr[0] << 8) + rr[1];
 	res->dns_rdata.srv.weight = (rr[2] << 8) + rr[3];
 	res->dns_rdata.srv.port = (rr[4] << 8) + rr[5];
-	dns_parse_domain(rr + 6, base, res, &res->dns_rdata.srv.server);
+	dns_parse_domain(rr + 6, base, max, res, &res->dns_rdata.srv.server);
 #ifdef DEBUG_DNSCLIENT
 	printf("SRV(server) = \"%s\".\n", res->dns_rdata.srv.server);
 	printf("SRV(weight) = %d.\n", res->dns_rdata.srv.weight);
@@ -376,7 +406,7 @@ dns_parse_results(const unsigned char *results, size_t length)
 	size_t skip;
 	int i;
 
-	if(length < sizeof(header)) {
+	if (length < sizeof(header)) {
 		return NULL;
 	}
 
@@ -387,33 +417,44 @@ dns_parse_results(const unsigned char *results, size_t length)
 	header.dns_nscount = ntohs(header.dns_nscount);
 	header.dns_arcount = ntohs(header.dns_arcount);
 
-	if(header.dns_qr != 1) { /* should be a response */
+	if (header.dns_qr != 1) { /* should be a response */
 		return NULL;
 	}
-	if(header.dns_rcode != 0) { /* should be no error */
+	if (header.dns_rcode != 0) { /* should be no error */
 		return NULL;
 	}
 
 	res = malloc(sizeof(struct dns_rr) * (header.dns_ancount + 1));
-	if(res == NULL) {
+	if (res == NULL) {
 		return NULL;
 	}
 	memset(res, 0, sizeof(struct dns_rr) * (header.dns_ancount + 1));
 
 	p = results + sizeof(header);
 
-	for(i = 0; i < header.dns_qdcount; i++) {
+	for (i = 0; i < header.dns_qdcount; i++) {
 		char *tmp;
 
-		skip = dns_parse_label(p, results + length - p, results,
+		if (p > results + length) {
+			free(res);
+			return NULL;
+		}
+
+		skip = dns_parse_label(p, results, length - (p - results),
 				       buf, sizeof(buf), NULL);
-		if(skip == 0) {
+		if (skip == 0) {
 			free(res);
 			return NULL;
 		}
 
 		tmp = strdup(buf);
 		p += skip;
+
+		if (p + 4 > results + length) {
+			free(res);
+			return NULL;
+		}
+
 		res[0].dns_type = (*p << 8) + *(p + 1);
 		p += 2;
 		res[0].dns_class = (*p << 8) + *(p + 1);
@@ -428,16 +469,22 @@ dns_parse_results(const unsigned char *results, size_t length)
 		free(tmp);
 	}
 
-	for(i = 0; i < header.dns_ancount; i++) {
-		skip = dns_parse_label(p, results + length - p, results,
+	for (i = 0; i < header.dns_ancount; i++) {
+		skip = dns_parse_label(p, results, length - (p - results),
 				       buf, sizeof(buf), NULL);
-		if(skip == 0) {
+		if (skip == 0) {
 			free(res);
 			return NULL;
 		}
 
 		res[i].dns_name = strdup(buf);
 		p += skip;
+
+		if (p + 10 > results + length) {
+			free(res);
+			return NULL;
+		}
+
 		res[i].dns_type = (p[0] << 8) + p[1];
 		p += 2;
 		res[i].dns_class = (p[0] << 8) + p[1];
@@ -449,8 +496,8 @@ dns_parse_results(const unsigned char *results, size_t length)
 		p += 2;
 
 #ifdef DEBUG_DNSCLIENT
-		fprintf(stderr, "Answer for '%s', class = %d, type = %d, "
-			"ttl = %d.\n",
+		fprintf(stderr, "Answer %d for '%s', class = %d, type = %d, "
+			"ttl = %d.\n", i,
 			res[i].dns_name, res[i].dns_class, res[i].dns_type,
 			res[i].dns_ttl);
 #endif
@@ -458,37 +505,70 @@ dns_parse_results(const unsigned char *results, size_t length)
 		rr = p;
 		switch(res[i].dns_type) {
 			case DNS_T_A:
-				dns_parse_a(rr, results, &res[i]);
+				dns_parse_a(rr,
+					    results,
+					    length - (p - results),
+					    &res[i]);
 				break;
 			case DNS_T_NS:
-				dns_parse_ns(rr, results, &res[i]);
+				dns_parse_ns(rr,
+					     results,
+					     length - (p - results),
+					     &res[i]);
 				break;
 			case DNS_T_CNAME:
-				dns_parse_cname(rr, results, &res[i]);
+				dns_parse_cname(rr,
+						results,
+						length - (p - results),
+						&res[i]);
 				break;
 			case DNS_T_SOA:
-				dns_parse_soa(rr, results, &res[i]);
+				dns_parse_soa(rr,
+					      results,
+					      length - (p - results),
+					      &res[i]);
 				break;
 			case DNS_T_NULL:
-				dns_parse_null(rr, results, &res[i]);
+				dns_parse_null(rr,
+					       results,
+					       length - (p - results),
+					       &res[i]);
 				break;
 			case DNS_T_WKS:
-				dns_parse_wks(rr, results, &res[i]);
+				dns_parse_wks(rr,
+					      results,
+					      length - (p - results),
+					      &res[i]);
 				break;
 			case DNS_T_PTR:
-				dns_parse_ptr(rr, results, &res[i]);
+				dns_parse_ptr(rr,
+					      results,
+					      length - (p - results),
+					      &res[i]);
 				break;
 			case DNS_T_HINFO:
-				dns_parse_hinfo(rr, results, &res[i]);
+				dns_parse_hinfo(rr,
+						results,
+						length - (p - results),
+						&res[i]);
 				break;
 			case DNS_T_MX:
-				dns_parse_mx(rr, results, &res[i]);
+				dns_parse_mx(rr,
+					     results,
+					     length - (p - results),
+					     &res[i]);
 				break;
 			case DNS_T_TXT:
-				dns_parse_txt(rr, results, &res[i]);
+				dns_parse_txt(rr,
+					      results,
+					      length - (p - results),
+					      &res[i]);
 				break;
 			case DNS_T_SRV:
-				dns_parse_srv(rr, results, &res[i]);
+				dns_parse_srv(rr,
+					      results,
+					      length - (p - results),
+					      &res[i]);
 				break;
 			case DNS_T_ANY:
 			default:
@@ -500,6 +580,11 @@ dns_parse_results(const unsigned char *results, size_t length)
 		}
 
 		p += res[i].dns_rlength;
+
+		if (p > results + length) {
+			free(res);
+			return NULL;
+		}
 	}
 
 	return res;
@@ -514,13 +599,13 @@ dns_client_init()
 	int tokens, ns;
 
 	ret = malloc(sizeof(struct dns_client_context));
-	if(ret == NULL) {
+	if (ret == NULL) {
 		return NULL;
 	}
 	memset(ret, 0, sizeof(struct dns_client_context));
 
 	ret->nameservers = malloc(sizeof(struct sockaddr_in*) * (SERVER_MAX + 1));
-	if(ret->nameservers == NULL) {
+	if (ret->nameservers == NULL) {
 		free(ret);
 		return NULL;
 	}
@@ -528,31 +613,31 @@ dns_client_init()
 	memset(ret->nameservers, 0, sizeof(struct sockaddr_in*) * (SERVER_MAX + 1));
 
 	fp = fopen(RESOLV_CONF, "r");
-	if(fp == NULL) {
+	if (fp == NULL) {
 		free(ret);
 		return NULL;
 	}
 
 	memset(buf, '\0', sizeof(buf));
-	while(fgets(buf, sizeof(buf), fp) != NULL) {
-		if((p = strchr(buf, '#')) != NULL) {
+	while (fgets(buf, sizeof(buf), fp) != NULL) {
+		if ((p = strchr(buf, '#')) != NULL) {
 			*p = '\0';
 		}
 
 		p = strtok_r(buf, WHITESPACE, &q);
-		if(p) {
-			if(!strcmp(p, "domain")) {
+		if (p) {
+			if (!strcmp(p, "domain")) {
 				p = strtok_r(NULL, WHITESPACE, &q);
 				ret->domain = strdup(p);
 			} else
-			if(!strcmp(p, "search")) {
+			if (!strcmp(p, "search")) {
 				ret->search = malloc(sizeof(char*) * (SEARCH_MAX + 1));
-				if(ret->search == NULL) {
+				if (ret->search == NULL) {
 					return NULL;
 				}
-				for(tokens = 0; tokens < (SEARCH_MAX + 1); tokens++) {
+				for (tokens = 0; tokens < (SEARCH_MAX + 1); tokens++) {
 					p = strtok_r(NULL, WHITESPACE, &q);
-					if(p) {
+					if (p) {
 						ret->search[tokens] = strdup(p);
 					} else {
 						ret->search[tokens] = NULL;
@@ -560,13 +645,13 @@ dns_client_init()
 					}
 				}
 			} else
-			if((!strcmp(p, "nameserver")) && (ns < SERVER_MAX)) {
+			if ((!strcmp(p, "nameserver")) && (ns < SERVER_MAX)) {
 				struct in_addr addr;
 				p = strtok_r(NULL, WHITESPACE, &q);
-				if(inet_aton(p, &addr) != 0) {
+				if (inet_aton(p, &addr) != 0) {
 					struct sockaddr_in *sin;
 					sin = malloc(sizeof(struct sockaddr_in));
-					if(sin == NULL) {
+					if (sin == NULL) {
 						return NULL;
 					}
 					memset(sin, 0, sizeof(struct sockaddr_in));
@@ -580,7 +665,7 @@ dns_client_init()
 					/* Try ipv6.  Don't know how to parse those just yet. */
 					sin6 = malloc(sizeof(struct sockaddr_in6));
 
-					if(sin6 == NULL) {
+					if (sin6 == NULL) {
 						return NULL;
 					}
 					memset(sin6, 0, sizeof(struct sockaddr_in6));
@@ -606,18 +691,34 @@ int
 main(int argc, char **argv)
 {
 	unsigned char buf[2048];
-	unsigned char answer[2048];
+	unsigned char *answer;
 	struct dns_rr *rr;
-	int ret;
+	int ret = 2048, len = 2048;
 
 	memset(&buf, 0, sizeof(buf));
 	ret = dns_format_query(argc > 1 ? argv[1] : "devserv.devel.redhat.com.",
 			       DNS_C_ANY, DNS_T_ANY, buf, sizeof(buf));
 	printf("Sending query.\n");
-	ret = res_send(buf, ret, answer, sizeof(answer));
+	answer = malloc(len);
+	do {
+		ret = res_send(buf, sizeof(buf), answer, len);
+		if (ret > len) {
+			free(answer);
+			len = ret + 1024;
+			answer = malloc(len);
+		} else {
+			break;
+		}
+	} while (1);
 	printf("Response was %d bytes long.\n", ret);
+	if (ret == -1) {
+		return 1;
+	}
 
 	rr = dns_parse_results(answer, ret);
+
+	free(answer);
+
 	return 0;
 }
 #endif
