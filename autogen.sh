931696f11cc2c8f3cd3b93f516a9a77ec97dc6ec
tmraz <devnull@localhost>
2005-03-04 03:01:45 -0600

    - changed version propagation, dist as .tar.bz2

--- autogen.sh
+++ autogen.sh
@@ -7,5 +7,7 @@ aclocal
 automake -a
 intltoolize -f
 autoconf
+VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
+sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
 test -x mkinstalldirs || cp /usr/share/gettext/mkinstalldirs .
 #./configure $@ --enable-maintainer-rules
