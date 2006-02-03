94f88711b5d7a6f4850de4889431962259f81e94
tmraz <devnull@localhost>
2006-02-03 16:17:31 -0600

    - --install needed as well of course

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 set -x
-autoreconf --force
+autoreconf --force --install
 VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
 sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
 test -x mkinstalldirs || cp /usr/share/gettext/mkinstalldirs .
