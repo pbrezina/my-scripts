7e6fbeaf04947a5af05b2f759c9ceda2ec91b533
tmraz <devnull@localhost>
2006-02-03 16:15:09 -0600

    - use autoreconf

--- autogen.sh
+++ autogen.sh
@@ -1,12 +1,5 @@
 set -x
-glib-gettextize -f -c
-touch config.h.in
-autoheader
-libtoolize -f -c
-aclocal
-automake -a
-intltoolize -f
-autoconf
+autoreconf --force
 VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
 sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
 test -x mkinstalldirs || cp /usr/share/gettext/mkinstalldirs .
