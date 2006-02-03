32e8e93924b8d00744c94dde6f48890cb9de132a
tmraz <devnull@localhost>
2006-02-03 16:22:43 -0600

    - add intltoolize back

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 set -x
+intltoolize -f -c
 autoreconf --force --install
 VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
 sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
 test -x mkinstalldirs || cp /usr/share/gettext/mkinstalldirs .
-#./configure $@ --enable-maintainer-rules
