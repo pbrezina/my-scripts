a52649770f5c7ddd9828873750c8f6de162114f5
tmraz <devnull@localhost>
2007-08-24 07:30:17 -0500

    - mkinstalldirs is no more in gettext

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,4 @@ intltoolize -f -c
 autoreconf --force --install
 VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
 sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
-test -x mkinstalldirs || cp /usr/share/gettext/mkinstalldirs .
+test -x mkinstalldirs || cp /usr/share/glib-2.0/gettext/mkinstalldirs .
