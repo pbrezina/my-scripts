894c52ea79c2cff10d51429b05d51e30a656c742
Tomas Mraz <tmraz@redhat.com>
2010-09-15 15:04:08 +0200

    Generate spec from configure directly.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,4 @@
 set -x
 intltoolize -f -c
 autoreconf --force --install
-VERSION=`sed '/AC_INIT/ !d; s/^.*,//; s/).*$//' configure.in`
-sed s/@PACKAGE_VERSION@/$VERSION/ authconfig.spec.in > authconfig.spec
 test -x mkinstalldirs || cp /usr/share/glib-2.0/gettext/mkinstalldirs .
